42e61125fa545cf4123f7e213938d258e098ce6a
Sebastian Duque <sebastian.duque221@tau.usbmed.edu.co>
2025-04-03 15:30:39 -0500

    Actualizar __init__.py en detecciones y evaluaciones para incluir todas las funciones disponibles

--- detecciones/__init__.py
+++ detecciones/__init__.py
@@ -1,2 +1,11 @@
 from .deteccion_saque import evaluar_saque
 from .deteccion_colocador import detectar_colocador
+from .deteccion_ataque import detectar_ataque
+from .deteccion_recibo import detectar_recibo
+
+__all__ = [
+    "evaluar_saque",
+    "detectar_colocador",
+    "detectar_ataque",
+    "detectar_recibo"
+]
--- evaluaciones/__init__.py
+++ evaluaciones/__init__.py
@@ -1,1 +1,11 @@
 from .evaluar_posicion import evaluar_posicion
+from .evaluar_contacto import evaluar_contacto
+from .evaluar_estabilidad import evaluar_estabilidad
+from .evaluar_sentadillas import evaluar_sentadillas
+
+__all__ = [
+    "evaluar_posicion",
+    "evaluar_contacto",
+    "evaluar_estabilidad",
+    "evaluar_sentadillas"
+]
