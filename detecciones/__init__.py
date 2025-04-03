34a0b78e154e63c4596d4248319e685e34e5a691
Sebastian Duque <sebastian.duque221@tau.usbmed.edu.co>
2025-04-03 16:55:33 -0500

    detecciones/init corregido

--- detecciones/__init__.py
+++ detecciones/__init__.py
@@ -1,10 +1,10 @@
-from .deteccion_saque import evaluar_saque
+from .deteccion_saque import detectar_saque
 from .deteccion_colocador import detectar_colocador
 from .deteccion_ataque import detectar_ataque
 from .deteccion_recibo import detectar_recibo
 
 __all__ = [
-    "evaluar_saque",
+    "detectar_saque",
     "detectar_colocador",
     "detectar_ataque",
     "detectar_recibo"
